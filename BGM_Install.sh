f1d1a57a50f81193f8630085b2b39c8739896681
madmodder123 <33565771+madmodder123@users.noreply.github.com>
2018-09-11 16:58:06 -0700

    Automatic install script!

---
+++ BGM_Install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# BGM.sh
+#############################################
+# Install background music + overlay
+#############################################
+
+cd /home/pi
+git clone https://github.com/madmodder123/retropie_music_overlay.git
+cd /home/pi/retropie_music_overlay
+sudo chmod +x pngview
+sudo cp pngview /usr/local/bin/
+sudo chmod +x BGM.py
+sudo cp BGM.py /home/pi/
+mkdir /home/pi/BGM/
+
+echo "Place your music files in /home/pi/BGM/ and then set /home/pi/BGM.py to run on boot!"
+echo "Edit /home/pi/BGM.py for more options!"
