53863b49a5a697bb0dd361a4db20dea8484f5699
madmodder123 <33565771+madmodder123@users.noreply.github.com>
2018-09-15 19:02:12 -0700

    Add files via upload

---
+++ BGM_Toggle.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# BGM_Toggle.sh
+#############################################
+# Lets the user enable/disable the background music
+#############################################
+
+if [ -f "/home/pi/DisableMusic" ]
+then
+	rm /home/pi/DisableMusic
+	(sudo python /home/pi/BGM.py) &
+else
+    echo -n "Hello" > /home/pi/DisableMusic
+	sudo pkill -f BGM.py
+	sudo pkill -f pngview
+fi
