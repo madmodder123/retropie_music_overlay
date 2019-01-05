b6610c47db61671299d2adb92146f36589402210
madmodder123 <33565771+madmodder123@users.noreply.github.com>
2019-01-04 21:39:01 -0800

    Update BGM_Toggle.sh

--- BGM_Toggle.sh
+++ BGM_Toggle.sh
@@ -4,12 +4,14 @@
 # Lets the user enable/disable the background music
 #############################################
 
-if [ -f "~/DisableMusic" ]
+if [ -f ~/DisableMusic ]
 then
 	rm ~/DisableMusic
-	(sudo python ~/BGM.py) &
+	python ~/BGM.py &
+	#echo "Enabled"
 else
     echo -n "Hello" > ~/DisableMusic
 	sudo pkill -f BGM.py
 	sudo pkill -f pngview
+	#echo "Disabled"
 fi
